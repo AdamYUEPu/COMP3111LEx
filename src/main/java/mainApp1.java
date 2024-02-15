9b83bd78cd666b7716bb6bad09e81d49e819141b
Adamyue <130567592+Adamyue@users.noreply.github.com>
2024-02-15 18:17:57 +0800

    Add 3rd print line

--- src/main/java/mainApp1.java
+++ src/main/java/mainApp1.java
@@ -2,6 +2,7 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("This is the 3rd line");
         System.out.println("Welcome to Scientific Calculator!");
         System.out.println("Program Started ...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b, n));
